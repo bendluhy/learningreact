95deaabb280fbcee520ccd8286e038964cbaca31
benjiboi703 <73977210+benjiboi703@users.noreply.github.com>
2021-06-20 23:17:57 -0400

    Initial Commit

---
+++ src/newTweet.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./App.css"
+function Tweet()
+{
+    return (
+        <div className="tweet">
+        <h1>Tweet Header</h1>
+        <p>Tweet Caption</p>
+        </div>
+        );
+}
+
+export default Tweet;
